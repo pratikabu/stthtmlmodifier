cc956a0ed806eb90d49576a9bcb09a1e18b06da5
Pratik Soni <pratikabu@gmail.com>
2013-07-26 21:11:51 +0530

    XML Modifier for Firefox

---
+++ src/com/pratikabu/scrolltotop/utils/GenericXMLModifier.java
@@ -0,0 +1,62 @@
+package com.pratikabu.scrolltotop.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class GenericXMLModifier {
+
+	/**
+	 * 
+	 * @param filePath
+	 * @param tagName
+	 * @param newValue
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws TransformerException
+	 */
+	public static void modifyTagValue(String filePath, String tagName, String newValue) throws Exception {
+		// initialize process
+		File fXmlFile = new File(filePath);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+		doc.getDocumentElement().normalize();
+
+		// update values
+		updateVersion(doc, tagName, newValue);
+
+		// save doc
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult streamResult = new StreamResult(new File(filePath));
+		transformer.transform(source, streamResult);
+	}
+
+	private static void updateVersion(Document doc, String tagName, String newValue) {
+		NodeList nList = doc.getElementsByTagName(tagName);
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				eElement.setTextContent(newValue);
+			}
+		}
+	}
+}
--- src/com/pratikabu/scrolltotop/utils/HtmlModifierDispatcher.java
+++ src/com/pratikabu/scrolltotop/utils/HtmlModifierDispatcher.java
@@ -1,19 +1,21 @@
 package com.pratikabu.scrolltotop.utils;
 
-import java.io.IOException;
 
 public class HtmlModifierDispatcher {
 	public static final String T_MODIFY_FIREFOX_OPTIONS = "MFO",
 			T_OPERA_CHROMIUM_OPTIONS_JS_IMPL = "OCOJI",
-			T_OPERA_STT_JS_EXPANDING = "OSJE";
+			T_OPERA_STT_JS_EXPANDING = "OSJE",
+			T_XML_MODIFER = "XMOD";
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		if(T_MODIFY_FIREFOX_OPTIONS.equals(args[0])) {
 			FirefoxHtmlModifier.processHtml(args[1], args[2]);
 		} else if(T_OPERA_CHROMIUM_OPTIONS_JS_IMPL.equals(args[0])) {
 			OperaChromiumJSModifer.processJS(args[1], args[2]);
 		} else if(T_OPERA_STT_JS_EXPANDING.equals(args[0])) {
 			OperaSTTJSModifer.processJS(args[1]);
+		} else if(T_XML_MODIFER.equals(args[0])) {
+			GenericXMLModifier.modifyTagValue(args[1], args[2], args[3]);
 		}
 	}
 	
